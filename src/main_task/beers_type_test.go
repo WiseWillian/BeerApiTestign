e2fa333fc40c095f1c586d75b8dd5a68b45568b4
WiseWill <rafael.willian.engcomp@gmail.com>
2018-01-31 23:45:19 -0200

    Correcao das constantes e transformacao da resposta json em array de bytes

--- src/main_task/beers_type_test.go
+++ src/main_task/beers_type_test.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 	"net/http"
 )
 
-const endpoint = "http://api.brewerydb.com/v2/beers/" //O endpoint da Api a ser consultado
-const apiKey = "key=47705820af1e5f9f31c6700101bc6494" //A chave da api cadastrada pelo desenvolvedor
+const endpoint = "http://api.brewerydb.com/v2/beers/?key=" //O endpoint da Api a ser consultado
+const apiKey = "47705820af1e5f9f31c6700101bc6494" //A chave da api cadastrada pelo desenvolvedor
 const chave = "styleId=1" //A pesquisa sera baseada pelo estilo da bebida. O Id 1 se refere à 'Classic English-Style Pale Ale'
 
-func TestMain(t *testing.T){
-	resp, err := http.Get(endpoint + "?" + apiKey + "&" + chave) //Faz a requisição para o endpoint
+func TestMain(t *testing.T) {
+	resp, err_req := http.Get(endpoint + apiKey + "&" + chave) //Faz a requisição para o endpoint
 
-	if err != nil { //Testa um eventual erro na requisição
-		t.Error("Erro ao solicitar informacao via GET para o endpoint")
+	//Testa um eventual erro na requisição
+	if err_req != nil { 
+		fmt.Println("Erro ao contatar o Endpoint: " + endpoint)
+		t.Error(err_req)
+	} else {
+		//Caso a requisição seja feita corretamente devemos processar a informacao
+		body, err_read := ioutil.ReadAll(resp.Body) //Transformamos a resposta em []Byte
+
+		//Testa um erro na leitura e transformação da resposta
+		if err_read != nil {
+			fmt.Println("Erro ao ler a resposta")
+			t.Error(err_read)
+		} else {
+			
+		}
 	}
 
 	defer resp.Body.Close()
