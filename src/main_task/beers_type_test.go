89537b6f88c7a00d89aeabba35a12715017e77b9
WiseWill <rafael.willian.engcomp@gmail.com>
2018-02-01 00:03:54 -0200

    Criacao das structs que padronizam o retorno da Api

--- src/main_task/beers_type_test.go
+++ src/main_task/beers_type_test.go
@@ -11,24 +11,79 @@ const endpoint = "http://api.brewerydb.com/v2/beers/?key=" //O endpoint da Api a
 const apiKey = "47705820af1e5f9f31c6700101bc6494" //A chave da api cadastrada pelo desenvolvedor
 const chave = "styleId=1" //A pesquisa sera baseada pelo estilo da bebida. O Id 1 se refere à 'Classic English-Style Pale Ale'
 
+type Category struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	CreateDate string `json:"createDate"`
+}
+
+type Available struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+}
+
+type Style struct {
+	Id int `json:"id"`
+	CategoryId int `json:"categoryId"`
+	StyleCategory Category `json:"category"`
+	Name string `json:"name"`
+	ShortName string `json:"shortName"`
+	Description string `json:"description"`
+	IbuMin string `json:"ibuMin"`
+	IbuMax string `json:"ibuMax"`
+	AbvMin string `json:"abvMin"`
+	AbvMax string `json:"abvMax"`
+	SrmMin string `json:"srmMin"`
+	SrmMax string `json:"srmMax"`
+	OgMin string `json:"ogMin"`
+	FgMin string `json:"fgMin"`
+	FgMax string `json:"fgMax"`
+	CreateDate string `json:"createDate"`
+	UpdateDate string `json:"updateDate"`
+}
+
+type Beer struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+	NameDisplay string `json:"nameDisplay"`
+	Description string `json:"description"`
+	Abv string `json:"abv"`
+	Ibu string `json:"ibu"`
+	AvailableId int `json:"availableId"`
+	StyleId int `json:"styleId"`
+	IsOrganic string `json:"isOrganic"`
+	Status string `json:"status"`
+	StatusDisplay string `json:"statusDisplay"`
+	CreateDate string `json:"createDate"`
+	UpdateDate string `json:"updateDate"`
+	BeerAvailable Available `json:"available"`
+	BeerStyle Style `json:"style"`
+}
+
+type BeerApiResponse struct {
+	CurrentPage int `json:"currentPage"`
+	NumberOfPages int `json:"numberOfPages"`
+	TotalResults int `json:"totalResults"`
+	Data []Beer `json:"data"`
+}
+
 func TestMain(t *testing.T) {
 	resp, err_req := http.Get(endpoint + apiKey + "&" + chave) //Faz a requisição para o endpoint
 
 	//Testa um eventual erro na requisição
 	if err_req != nil { 
 		fmt.Println("Erro ao contatar o Endpoint: " + endpoint)
-		t.Error(err_req)
-	} else {
-		//Caso a requisição seja feita corretamente devemos processar a informacao
-		body, err_read := ioutil.ReadAll(resp.Body) //Transformamos a resposta em []Byte
+		t.Fatal(err_req)
+	}
+	
+	//Caso a requisição seja feita corretamente devemos processar a informacao
+	body, err_read := ioutil.ReadAll(resp.Body) //Transformamos a resposta em []Byte
 
-		//Testa um erro na leitura e transformação da resposta
-		if err_read != nil {
-			fmt.Println("Erro ao ler a resposta")
-			t.Error(err_read)
-		} else {
-			
-		}
+	//Testa um erro na leitura e transformação da resposta
+	if err_read != nil {
+		fmt.Println("Erro ao ler a resposta")
+		t.Fatal(err_read)
 	}
 
 	defer resp.Body.Close()
